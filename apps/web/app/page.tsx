7d64182c23e7848292f08e6c1bec250263e7b954
clevermiraz <mirazhs@pm.me>
2025-10-03 07:21:08 +0600

    03: Clerk Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { useMutation, useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Apps/Web</h1>
-        <Button size="sm">{JSON.stringify(users)}</Button>
-        <Button onClick={() => addUser()}>Add</Button>
-      </div>
-    </div>
+  return ( 
+    <>
+      <Authenticated>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <p>apps/web</p>
+          <UserButton />
+          <Button onClick={() => addUser()}>Add User</Button>
+          <div className="max-w-3xl w-full mx-auto">{JSON.stringify(users, null, 2)}</div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Must be signed in!</p>
+        <SignInButton>Sign in</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -2,17 +2,17 @@
 
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Apps/Widget</h1>
-        <Button size="sm">{JSON.stringify(users)}</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <Button onClick={() => addUser()}>Add User</Button>
+      <div className="max-w-3xl w-full mx-auto">{JSON.stringify(users, null, 2)}</div>
     </div>
   );
 }
